3da65ace3438b977c554431293c6fa5df38ec8e4
hoangteoNaver <51512556+hoangteo0103@users.noreply.github.com>
2024-01-09 21:10:44 +0700

    Fix index.js bug

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import configureStore from './redux/store'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore()}>
-      <App />
+    <BrowserRouter>
+          <App />
+      </BrowserRouter>  
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
