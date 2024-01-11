6a406553e854c7c4aaa471a1fe348158b648410d
hoangteoNaver <51512556+hoangteo0103@users.noreply.github.com>
2024-01-11 17:24:27 +0700

    Update socket and create new game room

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,16 @@
 import rootReducer from "./reducers/rootReducer";
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
-import { Socket } from '../socket/socket'
-import {socketMiddleware} from '../socket/socketMiddleware'
+// import { Socket } from '../socket/socket'
+// import {socketMiddleware} from '../socket/socketMiddleware'
 
 export default function createStore(){
     return configureStore({
         reducer: rootReducer,
-        middleware: (getDefaultMiddleware) => [
-            ...getDefaultMiddleware(),
-            socketMiddleware(new Socket()),    
-        ],
+        // middleware: (getDefaultMiddleware) => [
+        //     ...getDefaultMiddleware(),
+        //     socketMiddleware(new Socket()),    
+        // ],
             devTools: process.env.NODE_ENV !== 'production',}
     )
 }
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,39 +1,10 @@
-class Socket {
-    constructor() {
-      this.socket = null
+import { io } from 'socket.io-client';
+import { socketURL } from '../constant';
 
-    }
-  
-    connect(url) {
-      if (!this.socket) {
-        this.socket = new WebSocket(url)
-      }
-    }
-  
-    disconnect() {
-      if (this.socket) {
-        this.socket.close()
-        this.socket = null
-      }
-    }
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = socketURL;
 
-    async send(message, payload) {
-    console.log('send', message, payload)
-    payload["event"] = message
-    if (this.socket.readyState) {
-        this.socket.send(JSON.stringify(payload))
-    } else {
-      setTimeout(() => {
-        this.send(message, payload)
-      }, 100);
+export const socket = io(URL, {
+    transports: ['websocket', 'polling']
     }
-  }
-  
-    on(eventName, callback) {
-      if (this.socket) {
-        this.socket.addEventListener(eventName, callback)
-      }
-    }
-  }
-  
-  export { Socket }
+    );
